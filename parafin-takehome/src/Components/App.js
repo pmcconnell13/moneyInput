fd7d7516dd9c7fa0989562b4c3f84dd4f612ef3f
Patrick McConnell <pmcconnell13@gmail.com>
2021-06-24 10:20:11 -0700

    working on string value

--- parafin-takehome/src/Components/App.js
+++ parafin-takehome/src/Components/App.js
@@ -7,38 +7,16 @@ function App() {
 
   const textFieldChange = (e) => {
     var character = e.target.value;
-    var commaAmount = Math.round((amount * 100) / 100).toLocaleString()
-    var stringAmount = amount.toString();
-    // if (stringAmount.length <= 3) {
-    //   updateAmount(character)
-    // } else if (stringAmount.length === 4) {
-    //   var firstComma = stringAmount.split('').splice(1, 0, ',').join('')
-    //   console.log('hi', firstComma)
-    //   updateAmount(character)
-    // }
-    console.log(Number(character))
-    if (Number(character)) {
-      updateAmount(character)
-    }
-    // if (stringAmount.length <= 4) {
-    //   updateAmount(character)
-    //   if (stringAmount.length === 4) {
-    //     updateCheck(amount);
-    //     updateAmount(commaAmount)
-    //   }
-    // }
-    // if (stringAmount.length === 5) {
-    //   console.log('hiii')
-    //   var newAmount = Math.round((check * 100) / 100).toLocaleString();
-    //   updateAmount(newAmount)
-    // }
-    console.log('amount', check)
+
+    console.log('parsed', character)
+    updateAmount(Math.round((parseInt(character) * 100) / 100).toLocaleString())
+    console.log('amount', amount)
   }
 
   return (
     <div className="App">
       <span className="inputSymbol">
-        <input className="amountInput" type="text" value={Math.round((amount * 100) / 100).toLocaleString()} onChange={textFieldChange} maxlength="7"></input>
+        <input className="amountInput" type="text" value={amount} onChange={textFieldChange} maxlength="6"></input>
       </span>
       {
         (amount > 30000 || amount < 10000 && amount > 0) &&
@@ -47,7 +25,7 @@ function App() {
         </div>
       }
       <div style={{"display": "flex", "flex-wrap": "wrap", "flex-direction": "column"}}>
-        <input className="rangeSlider" type="range" min="10000" max="30000" value={amount} onChange={e => updateAmount(e.target.value)} step="2000" ></input>
+        <input className="rangeSlider" type="range" min="10000" max="30000" value={amount} onChange={textFieldChange} step="2000" ></input>
         <div style={{"display": "flex", "color": "gray", "font-size": "12px"}}>
           <span> $10,000</span>
           <span style={{"display": "flex", "margin-left": "auto"}}>$30,000</span>
